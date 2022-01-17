81550b89fe024ab49912b7c138eab1cd40931ed2
luis-programming <programming@luis-moreira.com>
2022-01-17 21:12:23 +0000

    2 files modified, 1 added

--- src/app/generate-info/generate-info.component.ts
+++ src/app/generate-info/generate-info.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Info } from '../info';
 
+import { INFO } from '../info-mock';
+
 @Component({
   selector: 'app-generate-info',
   templateUrl: './generate-info.component.html',
   styleUrls: ['./generate-info.component.css'],
 })
 export class GenerateInfoComponent implements OnInit {
-  hero: Info = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  infos = INFO;
+
   constructor() {}
 
   ngOnInit() {}
+
+  selectedInfo?: Info;
+  onSelect(info: Info): void {
+    this.selectedInfo = info;
+  }
 }
